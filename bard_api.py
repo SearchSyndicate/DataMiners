5aa84b24f0eb4ea2a3625aa8ff8d3c646c4c4629
Muhamad-Nady <muhamednady20@gmail.com>
2023-05-22 12:33:48 +0300

    update about bard

--- bard_api.py
+++ bard_api.py
@@ -24,30 +24,24 @@ def bard_api(prompt):
         
     bard = Bard(session=session, timeout=30)
     ans = bard.get_answer(prompt)['content']
-    ans = ans.split("```")[1]
+    ans = ans.split("```json")[1]
     ans = ans.split("```")[0]
     return ans
 
 
 if __name__ == "__main__":
-    semantic_txt = '''Cleaning tools Bosch brings together comprehensive expertise in vehicle technology
-            with hardware software and services to offer complete mobility solutions Bosch offers innovative products and services for
-            industry and trades Bosch offers innovative products and services for industry and trades We look forward to your inquiry 
-            Explore Boschs wide range of products and solutions for your market and industry Heating cooling and wellbeing Power tools
-            Measuring tools Bosch brings together comprehensive expertise in vehicle technology with hardware software and services to 
-            offer complete mobility solutions Explore Boschs wide range of products and solutions for your market and industry Garden tools 
-            Home appliances Smart Home Find our PGP Key hereFingerprint F40C 0FE3 E919 B082 B2DD 75E5 929D 3AFD 217E 21D7 The Bosch
-            Product Security Incident Response Team PSIRT is the central point of contact for external security researchers partners
-            and customers to report cybersecurity information related to products developed by Bosch and its brands Responsible disclosure
-            of vulnerabilities has longerterm benefits because it allows us to fix vulnerabilities inform customers about fixes and
-            continuously improve security in our products If you believe you have identified a potential vulnerability or security 
-            issue in a Bosch product or service please contact us using our Vulnerability Reporting process We strongly encourage you
-            to encrypt all communication with the Bosch PSIRT Our SMIME and PGP public keys and fingerprints are available at the bottom
-            of each page Search our SMIME key hereFingerprint 87F16F7060D294838IKEA Family is for everyone. From those whose homes are
-            their passion, to those who are just starting out and need a helping hand. Just by being a member you’ll receive discounts
-            on many products! 2AC69F546867C807F861DF0'''
+    semantic_txt = '''Yandex is a technology company based in Russia that provides a wide range of online services and products. It was founded in 1997 by Arkady Volozh and Ilya Segalovich and has since grown to become one of the largest and most influential technology companies in Russia and neighboring countries.
+Here are some key aspects of Yandex:
+Search Engine: Yandex operates the largest search engine in Russia, accounting for the majority of web searches in the country. Similar to Google, Yandex offers search results, ads, maps, and other related services.
+Online Services: Yandex offers a variety of online services, including email (Yandex.Mail), cloud storage (Yandex.Disk), news (Yandex.News), translation (Yandex.Translate), video hosting (Yandex.Video), and more. These services cater to the needs of individual users as well as businesses.
+Ride-Hailing and Food Delivery: Yandex operates Yandex.Taxi, a popular ride-hailing service in Russia and other countries. Additionally, Yandex.Food provides online food delivery services in partnership with local restaurants.
+Advertising: Yandex has a significant presence in the digital advertising market. It offers advertising solutions through its advertising platform, including contextual ads, display ads, video ads, and more.
+Technology and AI: Yandex heavily invests in research and development, particularly in the field of artificial intelligence (AI). They have developed their own AI technologies, including machine learning algorithms, computer vision, speech recognition, and natural language processing.
+Maps and Navigation: Yandex.Maps provides mapping and navigation services, similar to Google Maps. It offers detailed maps, traffic information, and route planning features.
+E-commerce: Yandex operates Yandex.Market, an online marketplace where users can compare prices and make purchases from various online retailers.
+Yandex has established itself as a prominent player in the Russian technology industry, offering a wide range of services to millions of users. With its focus on search, online services, ride-hailing, and AI, Yandex continues to innovate and expand its presence both within Russia and internationally.'''
     
-    query = 'Bosch Germany'
+    query = 'Yandex'
     
     prompt = f"""
     Your task is to help a marketing team extract useful informations
@@ -59,13 +53,9 @@ if __name__ == "__main__":
         - list of Services offered by {query} separated by commas.
         - list of Keywords about the Products or Services of {query} 
           separated by commas.
-          
     2. You must identify atleast one item in text either from Products or Services.
-
     3. Make each item of the above lists one or two words long, if possible. 
-
     4. Make your response as short as possible without any explanation or notes.
-
     5. Format your response only as a JSON object with \
         "description","Products", "Services" and "Keywords" as the keys. 
         If the information isn't present in the test, use "unknown" \
@@ -74,7 +64,8 @@ if __name__ == "__main__":
     """
     
     response = bard_api(prompt=prompt)
-    try:
-        print(response)
-    except Exception as e:
-        print("exception")
+    for _ in range(10):
+        try:
+            print(response)
+        except Exception as e:
+            print(e)
